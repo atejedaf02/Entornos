1cf12a28b88221c2eed1cb5797e22ac6dd23d377
alumno <alumno@aula1-07>
2015-10-15 17:59:15 +0200

    Ejercicioa2

---
+++ PruebaGIT/src/pruebagit/ClaseA2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author alumno
+ */
+public class ClaseA2 {
+    public static void main(String[] args) {
+        System.out.println("Esto es el fichero de a2");//Soy Ivan
+    }
+}
