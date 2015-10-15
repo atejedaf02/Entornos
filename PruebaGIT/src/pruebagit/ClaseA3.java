b860979ef1aa057135f93db9992f4600f89833e4
efernandezr03 <efernandezr03@aula1-05>
2015-10-15 17:55:29 +0200

    Ejercicio03 Compartido

---
+++ PruebaGIT/src/pruebagit/ClaseA3.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author efernandezr03
+ */
+public class ClaseA3 {
+        public static void main(String[] args) {
+        System.out.println("Esto es el fichero de a3");//Soy eric
+    }
+}
