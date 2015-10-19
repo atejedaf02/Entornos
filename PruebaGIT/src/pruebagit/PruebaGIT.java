1a40ec8d08a8dc0427369be3641153414b37599e
atejedaf02 <atejedaf02@informatica.iesvalledeljerteplasencia.es>
2015-10-19 19:11:26 +0200

    Update PruebaGIT.java

--- PruebaGIT/src/pruebagit/PruebaGIT.java
+++ PruebaGIT/src/pruebagit/PruebaGIT.java
@@ -15,7 +15,7 @@ public class PruebaGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   
+      System.out.println("Esto es una prueba");//admin
         System.out.println("Rama Álvaro");
     }
     
